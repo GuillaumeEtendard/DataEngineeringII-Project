fff85799cf8867909ee135702d21271ab6cc3565
Tom TEA <tomteafrance@gmail.com>
2021-12-10 17:26:27 +0100

    [DEP-18] modify unittest

--- back/app_test.py
+++ back/app_test.py
@@ -3,9 +3,10 @@ import requests
 
 class Test(unittest.TestCase):
     def test_response(self):
-        response = requests.get('http://localhost:8000/get_sentiment/')
+        params = {'text':'i love chocolat !'}
+        response = requests.get('http://localhost:8000/get_sentiment/',params = params)
         print(response.elapsed.total_seconds())
-        self.assertLessEqual(response.elapsed.total_seconds(), 1) 
+        self.assertLessEqual(response.elapsed.total_seconds(), 5) 
 
 if __name__ == '__main__':
     unittest.main()
